65d1aaa63e41dbd80346852b03b1c70dfb254f4c
Gordon <gor.rennie@gmail.com>
2018-10-21 12:32:12 +0100

    Clean up code style, add missing getter

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -10,6 +10,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+/**
+ * Initialises basic data on Spring start-up.
+ */
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
